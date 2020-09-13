bd3bec442e03d84ebecff82ab222714a2514e87a
Guido1956 <GuidoDulos@valori.nl>
2020-09-13 20:38:26 +0200

    Versie 1 Autoclass met 1 constructor

---
+++ src/main/java/Auto.java
@@ -0,0 +1,35 @@
+public class Auto {
+    String kenteken;
+    String merk;
+    String kleur;
+
+    public Auto(String kenteken, String merk, String kleur) {
+        this.kenteken = kenteken;
+        this.merk = merk;
+        this.kleur = kleur;
+    }
+
+    public String getKenteken() {
+        return kenteken;
+    }
+
+    public void setKenteken(String kenteken) {
+        this.kenteken = kenteken;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(String kleur) {
+        this.kleur = kleur;
+    }
+}
