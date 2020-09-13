9b9edf2aba7773f1e1c27f893f0d3e229ea99c5e
Guido1956 <GuidoDulos@valori.nl>
2020-09-13 20:59:05 +0200

    Versie 2 Team - Main met lab-testgeval

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto testAuto = new Auto ("AA-000-B", "MERK", "KLEUR");
+        System.out.println(testAuto.toString());
+    }
+}
